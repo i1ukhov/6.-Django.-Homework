f34ad3f179bfd3049f77384a2e3d458a73ea5a67
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-05-07 19:51:39 +0600

    all tasks done

--- Homework 2/catalog/urls.py
+++ Homework 2/catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import products_list, contacts, product_detail
 
 app_name = CatalogConfig.name
+
+
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('', products_list, name='products_list'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>', product_detail, name='product_detail')
 ]
--- Homework 2/catalog/views.py
+++ Homework 2/catalog/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Category, Product
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
 
 
 def contacts(request):
