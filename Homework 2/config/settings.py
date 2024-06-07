bfef8d4534bedcf0ff8c62e712c46216c2ab9952
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-06-07 17:42:48 +0600

    installing packages

--- Homework 2/config/settings.py
+++ Homework 2/config/settings.py
@@ -141,3 +141,6 @@ FIXTURES_ROOT = os.path.join(BASE_DIR, 'fixtures')
 
 AUTH_USER_MODEL = 'users.User'
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
--- Homework 2/users/urls.py
+++ Homework 2/users/urls.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'),)
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LoginView.as_view(), name='logout')
 ]
